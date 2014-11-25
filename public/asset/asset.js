18bafb4be69f5c02923a60347e0065443e047ecc
haiyang5210 <haiyang5210@gmail.com>
2014-11-26 07:15:31 +0800

    Signed-off-by: haiyang5210 <haiyang5210@gmail.com>

--- public/asset/asset.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-document.write('<script type="text/javascript" src="src/jquery1.9.1.js         "></script>');
-document.write('<script type="text/javascript" src="src/hui.js                 "></script>');
-document.write('<script type="text/javascript" src="src/highcharts.js          "></script>');
-document.write('<script type="text/javascript" src="src/highcharts-more.js     "></script>');
